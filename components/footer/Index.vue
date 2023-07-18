04cce9639bcafb2bc8b3590798625ddb6358b494
YannickBG92 <42341175+snowart92@users.noreply.github.com>
2023-07-18 15:41:02 +0200

    create footer

--- components/footer/Index.vue
+++ components/footer/Index.vue
@@ -1,6 +1,55 @@
 <template>
   <div class="flex flex-col h-full">
-    <footer class="mt-auto">Footer</footer>
+    <footer class="mt-auto">
+      <div
+        class="max-w-3xl mx-auto p-4 grid grid-cols-2 lg:max-w-7xl lg:grid-cols-6 md:py-10"
+      >
+        <div class="m-3 md:m-5">
+          <h5 class="mb-3 text-gray-900 text-lg font-semibold sm:text-xl">
+            Best food
+          </h5>
+          <ul>
+            <li class="my-2 text-gray-800 text-md sm:text-lg">test 1</li>
+            <li class="my-2 text-gray-800 text-md sm:text-lg">test 2</li>
+            <li class="my-2 text-gray-800 text-md sm:text-lg">test 3</li>
+            <li class="my-2 text-gray-800 text-md sm:text-lg">test 4</li>
+          </ul>
+        </div>
+        <div class="m-3 md:m-5">
+          <h5 class="mb-3 text-gray-900 text-lg font-semibold sm:text-xl">
+            Best food
+          </h5>
+          <ul>
+            <li class="my-2 text-gray-800 text-md sm:text-lg">test 1</li>
+            <li class="my-2 text-gray-800 text-md sm:text-lg">test 2</li>
+            <li class="my-2 text-gray-800 text-md sm:text-lg">test 3</li>
+            <li class="my-2 text-gray-800 text-md sm:text-lg">test 4</li>
+          </ul>
+        </div>
+        <div class="m-3 md:m-5">
+          <h5 class="mb-3 text-gray-900 text-lg font-semibold sm:text-xl">
+            Best food
+          </h5>
+          <ul>
+            <li class="my-2 text-gray-800 text-md sm:text-lg">test 1</li>
+            <li class="my-2 text-gray-800 text-md sm:text-lg">test 2</li>
+            <li class="my-2 text-gray-800 text-md sm:text-lg">test 3</li>
+            <li class="my-2 text-gray-800 text-md sm:text-lg">test 4</li>
+          </ul>
+        </div>
+        <div class="m-3 md:m-5">
+          <h5 class="mb-3 text-gray-900 text-lg font-semibold sm:text-xl">
+            Best food
+          </h5>
+          <ul>
+            <li class="my-2 text-gray-800 text-md sm:text-lg">test 1</li>
+            <li class="my-2 text-gray-800 text-md sm:text-lg">test 2</li>
+            <li class="my-2 text-gray-800 text-md sm:text-lg">test 3</li>
+            <li class="my-2 text-gray-800 text-md sm:text-lg">test 4</li>
+          </ul>
+        </div>
+      </div>
+    </footer>
   </div>
 </template>
 
