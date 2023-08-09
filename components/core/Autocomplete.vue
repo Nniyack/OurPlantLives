b184df1280931c611568b64b1e3f75e8db0a6808
YannickBG92 <42341175+snowart92@users.noreply.github.com>
2023-08-09 16:48:56 +0200

    create filter numPerPage and type order

--- components/core/Autocomplete.vue
+++ components/core/Autocomplete.vue
@@ -1,7 +1,7 @@
 <template>
   <div ref="autocompleteField" class="w-full">
     <div
-      class="bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 rounded-md focus:ring-1"
+      class="bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none rounded-md focus:ring-1"
     >
       <label class="relative block">
         <span
@@ -16,7 +16,7 @@
         <input
           type="text"
           :name="nameProps"
-          class="pr-9 px-3 py-2 bg-white placeholder-slate-400 focus:outline-none block w-[300px] rounded-md sm:text-sm w-full"
+          class="pr-9 px-3 py-2 bg-white placeholder-slate-400 focus:outline-none block rounded-md sm:text-sm w-full"
           placeholder="Recherche ..."
           :value="searchedValue"
           @input="inputField"
@@ -37,7 +37,7 @@
         </div>
       </div>
     </div>
-    <div v-on:focusout="outsideClick">
+    <div ref="optionsList">
       <ul
         v-if="openListResultsSearch"
         class="list-none text-sm mt-2 border absolute z-10 drop-shadow-lg max-h-[300px] w-[300px] overflow-auto field-search-content"
@@ -59,6 +59,7 @@
   import { defineComponent, ref } from "vue";
   import { v4 as uuidv4 } from "uuid";
   import { XMarkIcon } from "@heroicons/vue/24/outline";
+  import { onClickOutside } from "@vueuse/core";
 
   export default defineComponent({
     props: {
@@ -72,6 +73,7 @@
     setup(props: any, context: any) {
       const { name: nameProps } = props;
       const autocompleteField = ref(null);
+      const optionsList = ref(null);
       const openListResultsSearch = ref(false);
       const loadedData = ref(props.dataLoad);
       const resultDataFiltered = ref([]);
@@ -130,9 +132,7 @@
         context.emit("returnObject", fieldValue.value);
       };
 
-      const outsideClick = () => {
-        openListResultsSearch.value = false;
-      };
+      onClickOutside(optionsList, () => (openListResultsSearch.value = false));
 
       return {
         props,
@@ -145,9 +145,9 @@
         openListResultsSearch,
         fieldValue,
         removeValueField,
-        outsideClick,
         autocompleteField,
         removePills,
+        optionsList,
       };
     },
   });
