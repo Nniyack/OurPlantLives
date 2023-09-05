6f72bc4c7736e64dca29f5eb9fd87394da833baa
YannickBG92 <42341175+snowart92@users.noreply.github.com>
2023-09-05 17:41:06 +0200

    add modal authentification

---
+++ front/components/home/Modal/Authentification.vue
@@ -0,0 +1,35 @@
+<template>
+  <div v-if="props.show">
+    <p>Hello from the modal!</p>
+    <button @click="open = false">Close</button>
+  </div>
+</template>
+
+<script lang="ts">
+  import { defineComponent, watch } from "vue";
+
+  export default defineComponent({
+    props: {
+      show: Boolean,
+    },
+    setup(props: any) {
+      const { show } = props;
+      console.log(props);
+      watch(props, () => {
+        console.log(show);
+      });
+      return { props };
+    },
+  });
+</script>
+
+<style scoped>
+.modal {
+  position: fixed;
+  z-index: 999;
+  top: 20%;
+  left: 50%;
+  width: 300px;
+  margin-left: -150px;
+}
+</style>
--- front/nuxt.config.ts
+++ front/nuxt.config.ts
@@ -1,24 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  runtimeConfig: {
-    apiKey: process.env.apiKey,
-    authDomain: process.env.authDomain,
-    projectId: process.env.projectId,
-    public: {
-      firebase: {
-        apiKey: process.env.apiKey,
-        authDomain: process.env.authDomain,
-        projectId: process.env.projectId,
-        storageBucket: process.env.storageBucket,
-        messagingSenderId: process.env.messagingSenderId,
-        appId: process.env.appId
-      }
-    }
-  },
   css: [
     '@/assets/css/main.css',
   ],
+  runtimeConfig: {
+    public: {
+      FB_API_KEY: process.env.FB_API_KEY,
+      FB_AUTH_DOMAIN: process.env.FB_AUTH_DOMAIN,
+      FB_PROJECT_ID: process.env.FB_PROJECT_ID,
+      FB_STORAGE_BUCKET: process.env.FB_STORAGE_BUCKET,
+      FB_MESSAGING_SENDER_ID: process.env.FB_MESSAGING_SENDER_ID,
+      FB_APP_ID: process.env.FB_APP_ID,
+      FB_MEASUREMENT_ID: process.env.FB_MEASUREMENT_ID,
+    }
+  },
   postcss: {
     plugins: {
       tailwindcss: {},
